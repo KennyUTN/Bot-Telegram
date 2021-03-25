8acf362806bd30822b81ff9a3fd7b760a6fa4563
KennyUTN <asisignacio@gmail.com>
2021-03-25 20:47:54 -0300

    Review fixes

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,30 +1,20 @@
 require 'telegram/bot'
-
+require_relative '../lib/quotes.rb'
 class PhiloBot
   def initialize
-    token = 'YOUR_TOKEN_KEY'
+    token = '1784667261:AAFt12WwsdAk7CDF8UPp57a5PA6E0cGRINg'
+    @quotes = Quotes.new
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |msg|
         # rubocop:disable Layout/LineLength
-        happyquote = ['Be yourself; everyone else is already taken',
-                      'To live is the rarest thing in the world. Most people exist, that is all.',
-                      'Live as if you were to die tomorrow. Learn as if you were to live forever.',
-                      'Great minds discuss ideas; average minds discuss events; small minds discuss people.',
-                      'Do one thing every day that scares you'].sample
-
-        sadquote = ['Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate; only love can do that.',
-                    'Our lives begin to end the day we become silent about things that matter.',
-                    'In the end, we will remember not the words of our enemies, but the silence of our friends.',
-                    "I have not failed. I've just found 10,000 ways that won't work.",
-                    'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.'].sample
 
         message = "My fellow friend, send :'Im Happy' for a happy quote.
       Otherwhise send 'Im Sad' so you can recieve a proper quote."
         # rubocop:enable Layout/LineLength
 
-        message = happyquote if msg.text == 'Im Happy'
+        message = @quotes.get_happy_quote if msg.text == 'Im Happy'
 
-        message = sadquote if msg.text == 'Im Sad'
+        message = @quotes.get_sad_quote if msg.text == 'Im Sad'
 
         bot.api.send_message(chat_id: msg.chat.id, text: message)
       end
---
+++ spec/bot_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/quotes.rb'
+describe Quotes do
+  let(:test) { Quotes.new }
+
+
+describe '#get_happy_quote' do
+  context 'Gets a random string'
+  subject {test.get_happy_quote}
+  it {is_expected.to be_kind_of(String) }
+end
+
+describe '#get_sad_quote' do
+  context 'Gets a random sad string'
+  subject {test.get_sad_quote}
+  it {is_expected.to be_kind_of(String)}
+end
+
+end
