52b74056e627cc5653b022fb82f13e25281cbc9a
KennyUTN <asisignacio@gmail.com>
2021-03-25 21:01:18 -0300

    spec linters

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -2,17 +2,15 @@ require_relative '../lib/quotes.rb'
 describe Quotes do
   let(:test) { Quotes.new }
 
+  describe '#get_happy_quote' do
+    context 'Gets a random string'
+    subject { test.a_happy_quote }
+    it { is_expected.to be_kind_of(String) }
+  end
 
-describe '#get_happy_quote' do
-  context 'Gets a random string'
-  subject {test.a_happy_quote}
-  it {is_expected.to be_kind_of(String) }
-end
-
-describe '#get_sad_quote' do
-  context 'Gets a random sad string'
-  subject {test.a_sad_quote}
-  it {is_expected.to be_kind_of(String)}
-end
-
+  describe '#get_sad_quote' do
+    context 'Gets a random sad string'
+    subject { test.a_sad_quote }
+    it { is_expected.to be_kind_of(String) }
+  end
 end
